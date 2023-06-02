5499ba44188910e0adc2e297468dd76d4b0dd49a
coutinhoou <111984834+coutinhoou@users.noreply.github.com>
2023-06-02 10:41:15 -0300

    Add files via upload

--- index.php
+++ index.php
@@ -14,83 +14,109 @@
       margin: 0;
       padding: 0;
       font-family: 'Garamond', sans-serif;
+      background-color: #f1f1f1; /* Cor de fundo igual ao h1 */
     }
 
     header {
       width: 100%;
+      height: 80px; /* Altura do cabeçalho reduzida */
       padding: 20px 0;
-      background-color: #f1f1f1;
+      background-color: #000;
+      display: flex;
+      align-items: center;
+    }
+
+    .logo {
+      max-width: 150px;
+      margin-left: 20px;
     }
 
     h1 {
-      font-size: 24px;
+      font-size: 40px;
       font-weight: bold;
-      margin: 0;
+      margin: 0 auto;
+      color: #fff;
     }
 
-    nav ul {
-      list-style-type: none;
-      padding: 0;
+    .header-buttons {
+      margin-left: auto;
+      display: flex;
+      align-items: center;
+    }
+
+    .header-buttons a {
+      color: #fff;
+      margin-left: 10px;
+      text-decoration: none;
+    }
+
+    .carousel {
+      display: flex;
+      justify-content: center;
+      align-items: center;
       margin: 20px 0;
     }
 
-    nav ul li {
-      display: inline-block;
-      margin-right: 10px;
+    .carousel-item {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      margin: 0 20px;
     }
 
-    nav ul li a {
-      text-decoration: none;
-      color: #333;
-    }
-
-    .search-bar {
-      margin-top: 20px;
-    }
-
-    .search-bar input[type="text"] {
-      padding: 5px;
-    }
-
-    .search-bar input[type="submit"] {
-      padding: 5px 10px;
-      background-color: #333;
-      color: #fff;
-      border: none;
+    .carousel-item img {
+      width: 80px;
+      height: 80px;
+      margin-bottom: 10px;
       cursor: pointer;
+    }
+
+    .carousel-item span {
+      font-size: 14px;
+    }
+
+    footer {
+      width: 100%;
+      padding: 20px 0;
+      background-color: #000;
+      color: #fff;
     }
   </style>
 </head>
 <body>
   <header>
-    <h1 style="font-size: 50px;">ADEGA MILITROS</h1>
-    <nav>
-      <ul>
-        <li><a href="home.php">Home</a></li>
-        <li><a href="produtos.php">Cervejas</a></li>
-        <li><a href="carrinho.php">Combos</a></li>
-        <li><a href="contato.php">Comentários</a></li>
-      </ul>
-      <form class="search-bar" action="pesquisa.php" method="GET">
-        <input type="text" name="pesquisa" placeholder="Pesquisar...">
-        <input type="submit" value="Buscar">
-      </form>
-    </nav>
-  </header> 
+    <img class="logo" src="logo.png" alt="Logo">
+    <div style="flex: 1;">
+    <h1 style="font-size: 40px; color: #fff;">ADEGA MILITROS</h1>
+    </div>
+    <div class="header-buttons">
+      <a href="#">Contato</a>
+      <a href="#">Ajuda</a>
+      <a href="#">Redes Sociais</a>
+    </div>
+  </header>
 
   <section>
-    <h2>Produtos</h2>
-    <div class="product-card">
-      <img src="/img/vinho1.png" alt="Vinho 1">
-      <h3>Vinho 1</h3>
-      <p>Descrição do vinho 1.</p>
-      <a href="detalhes.php?id=1">Ver detalhes</a>
-    </div>
-    <div class="product-card">
-      <img src="vinho1.png" alt="Vinho 1">
-      <h3>Vinho 2</h3>
-      <p>Descrição do vinho 2.</p>
-      <a href="detalhes.php?id=2">Ver detalhes</a>
+    <h2 style="font-size: 40px; color: #000;">Produtos</h2>
+    <div class="carousel">
+      <div class="carousel-item">
+        <h1 style="font-size: 30px; color: #000;">cervejas</h1>
+        <a href="cervejas.php">
+          <img src="cerveja-icon.png" alt="Cervejas">
+        </a>
+      </div>
+      <div class="carousel-item">
+        <h1 style="font-size: 30px; color: #000;">destilados</h1>
+        <a href="destilados.php">
+          <img src="destilados-icon.png" alt="Destilados">   
+        </a>
+      </div>
+      <div class="carousel-item">
+        <h1 style="font-size: 30px; color: #000;">combos</h1>
+        <a href="combos.php">
+          <img src="combos-icon.png" alt="Combos">
+        </a>
+      </div>
     </div>
   </section>
 
